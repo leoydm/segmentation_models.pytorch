ba0a97a180a96987cd0fcdcfb8a1a32f4e97057f
Leo <13986467+leoydm@users.noreply.github.com>
2025-10-03 00:12:04 +0800

    Model: fix diffs

--- segmentation_models_pytorch/decoders/deeplabv3/__init__.py
+++ segmentation_models_pytorch/decoders/deeplabv3/__init__.py
@@ -1,3 +1,3 @@
-from .model import DeepLabV3, DeepLabV3Plus, DeepLabV3Diff, DeepLabV3PlusDiff, DeepLabV3Refiner, DeepLabV3PlusRefiner, DeepLabV3PlusDiffS, DeepLabV3PlusMinus
+from .model import DeepLabV3, DeepLabV3Plus, DeepLabV3Diff, DeepLabV3PlusDiff, DeepLabV3Refiner, DeepLabV3PlusRefiner, DeepLabV3PlusMinus
 
-__all__ = ["DeepLabV3", "DeepLabV3Plus", "DeepLabV3Diff", "DeepLabV3PlusDiff", "DeepLabV3Refiner", "DeepLabV3PlusRefiner", "DeepLabV3PlusDiffS", "DeepLabV3PlusMinus"]
+__all__ = ["DeepLabV3", "DeepLabV3Plus", "DeepLabV3Diff", "DeepLabV3PlusDiff", "DeepLabV3Refiner", "DeepLabV3PlusRefiner", "DeepLabV3PlusMinus"]
